ad461c31aab04be603df7f8a3581f8006fbee907
Eric Ostring <eric.e.ostring@gmail.com>
2022-05-10 22:58:44 +0100

    Drop files from .gitignore

--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ericostring/Desktop/code_along/demo_rpg-ep12/googletest/googletest
-# Build directory: /Users/ericostring/Desktop/code_along/demo_rpg-ep12/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
